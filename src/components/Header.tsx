3b9fc56fab63cf438f6881007afa374e955f0b9d
David Kågell <David.kagell@hotmail.com>
2026-09-07 09:44:59 +0200

    company logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,27 @@
+import Image from "next/image";
 import { useTranslations } from "next-intl";
 import HeaderSearch from "@/components/HeaderSearch";
+import { org } from "@/config/org";
 import { Link } from "@/i18n/navigation";
 
 export default function Header() {
   const t = useTranslations("nav");
 
   return (
-    <header className="flex flex-row items-center justify-between gap-4 border-b-2 px-4 sm:px-10">
+    <header className="flex flex-row items-center justify-between gap-4 border-b-2 px-4 sm:px-10 bg-blue-200">
+      <div className="flex flex-row items-center justify-center gap-8">
+
+      <Link href="/" className="shrink-0">
+      
+        <Image
+          src="/images/products/company-logo.png"
+          alt={org.name}
+          width={64}
+          height={64}
+          priority
+          className="h-16 w-16  object-cover"
+        />
+      </Link>
       <nav>
         <ul className="flex h-20 flex-row items-center gap-5">
           <li>
@@ -23,6 +38,7 @@ export default function Header() {
           </li>
         </ul>
       </nav>
+      </div>
       <HeaderSearch />
     </header>
   );
